b8aaabcdbc16c402dc2e835cfd3bd8b2b73217d7
Regina <nigmatullinaregina@gmail.com>
2022-02-19 18:08:23 +0400

    fixed inline array annotation for modal controller

--- src/app/file/file-list-item-modal/file-list-item-modal.controller.js
+++ src/app/file/file-list-item-modal/file-list-item-modal.controller.js
@@ -1,11 +1,14 @@
 "use strict";
 
-angular
-  .module("file")
-  .controller("fileListItemModal", function ($scope, $uibModalInstance, file) {
+angular.module("file").controller("fileListItemModal", [
+  "$scope",
+  "$uibModalInstance",
+  "file",
+  function ($scope, $uibModalInstance, file) {
     $scope.file = file;
 
     $scope.cancel = function () {
       $uibModalInstance.dismiss("cancel");
     };
-  });
+  },
+]);
